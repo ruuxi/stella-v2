76ae3b084b72ac60807af175b720e1522218e872
ruuxi <lolruuxi@gmail.com>
2026-05-08 18:24:53 -0700

    Revert "Build GR canvas as a Stella app"
    This reverts commit 2f9f99837ade3c9a16f92f40d10dc7d5d7dad1bd.

--- desktop/src/routes/gr-canvas.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { GrCanvasApp } from "@/app/gr-canvas/App";
-
-export const Route = createFileRoute("/gr-canvas")({
-  component: GrCanvasApp,
-});
